4dc69d426b6904b963f0abb72b5ad02fdd1c95ba
henrisama <pedropiresflores@icloud.com>
2021-10-09 22:30:37 -0300

    gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import GlobalStyle from '../styles/globals'
+import { useRouter } from 'next/dist/client/router'
+import * as gtag from '../lib/gtag'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL): void => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Head>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ import Document, {
   NextScript
 } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 export default class MyDocument extends Document {
   static async getInitialProps (ctx: DocumentContext): Promise<DocumentInitialProps> {
@@ -67,6 +68,19 @@ export default class MyDocument extends Document {
                       url: 'https://x-caser.vercel.app/',
                       name: 'xCaser - Online String Converter'
                     })
+                  }}
+                />
+
+              {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></script>
+              <script
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+                      gtag('config', '${GA_TRACKING_ID}', { page_path: window.location.pathname });
+                    `
                   }}
                 />
 
